a2e8a202c6a5c1e82c6e8bb039d018458876c522
Tyrone <Tyrone@192.168.0.102>
2016-10-13 18:57:32 -0600

    Removed some comments

--- src/client/Main.java
+++ src/client/Main.java
@@ -9,30 +9,12 @@ import html_client.UrlCacheException;
 public class Main {
 	
 	public static void main(String[] args) {
-
-//		try{
-//			UrlCache cache = new UrlCache();
-//			
-//			//cache.getLastModified("people.ucalgary.ca/~mghaderi/index.html");
-//			cache.getObject("people.ucalgary.ca/~mghaderi/index.html");
-//			//cache.getLastModified("people.ucalgary.ca/~mghaderi/test/uc.gif");
-//			//cache.getLastModified("https://www.cbc.ca/radio/podcasts/index.html");
-//			cache.Close();
-//		}catch(UrlCacheException ex)
-//		{
-//			
-//		}
-//		
 		
-		// include whatever URL you like
-		// these are just some samples
 		String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
 						"people.ucalgary.ca/~mghaderi/test/uc.gif",
 						"people.ucalgary.ca/~mghaderi/test/a.pdf",
 						"people.ucalgary.ca:80/~mghaderi/test/test.html"};
 		
-		// this is a very basic tester
-		// the TAs will use a more comprehensive set of tests
 		try {
 			UrlCache cache = new UrlCache();
 			
